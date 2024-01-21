b8e687759a83e12759a5c175c741277c6b07b1fd
angrycoding <matveev.ruslan@gmail.com>
2024-01-21 21:12:17 +0300

    cleanup

--- index.ts
+++ index.ts
@@ -8,29 +8,15 @@ const readFileUint8 = (path: string) => {
 
 const fragmenter = new Fragmenter();
 
-
-const buf = [
-	fragmenter.push(readFileUint8('./videofrags/1.mp4')),
-	fragmenter.push(readFileUint8('./videofrags/2.mp4')),
-	fragmenter.push(readFileUint8('./videofrags/3.mp4')),
-]
-
-
-// const buf = [];
-// for (let c = 0; c <= 0; c++) {
-// 	const fragment = fragmenter.push(readFileUint8(`./test-1minute/output${String(c).padStart(3, '0')}.mp4`));
-// 	buf.push(fragment);
-// }
-
-FS.writeFileSync('./0.mp4', Buffer.concat(buf));
-
-// FS.writeFileSync('./0.mp4', Buffer.concat([
-// 	fragmenter.push(readFileUint8('./videofrags/1.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/2.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/3.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/4.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/5.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/6.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/7.mp4')),
-// 	fragmenter.push(readFileUint8('./videofrags/8.mp4')),
-// ]));
+FS.writeFileSync('./0.mp4', Buffer.concat([
+	fragmenter.push(readFileUint8('./videofrags/0.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/1.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/2.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/3.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/4.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/5.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/6.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/7.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/8.mp4')).data,
+	fragmenter.push(readFileUint8('./videofrags/9.mp4')).data,
+]));
